4cbead4904d9300442f9c205435c55c0640b83b6
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-09-27 08:44:23 -0400

    fourth commit

--- index.php
+++ index.php
@@ -3,21 +3,32 @@
 <body>
 <title>Week3 Assignment</title>
 <h1>Homework 3</h1>
-<h2>Adding Backslash In Front of Specified Character</h2>
+<hr>
+<h3>Adding Backslash In Front of Specified Character</h3>
 <?php
 $str = addcslashes("Hi, My name is Sanchit Gupta and I am pursuing my masters
 in Information systems","S");
 echo($str);
 ?>
+<hr>
 <h3>Get The Length Of The String of above mentioned syntax</h3>
 <?php
 echo strlen("Hi, My name is Sanchit Gupta and I am pursuing my masters in Information Systems");
 ?>
-<h4>Breaking a string into an  Array</h4>
+<hr>
+<h3>Breaking a string into an  Array</h3>
 <?php
 $str = "I currently have three courses in my first semester.";
 print_r (explode(" ",$str));
 ?>
+<hr>
+<h3>Replacing some characters in the string</h3>
+<?php
+echo str_ireplace("masters","masters of science","Hi, My name is Sanchit Gupta and
+I am pursuing my masters in Information Systems");
+?>
+<p>" In this string function I replaced 'masters' to 'masters of science' from the
+above mentioned line."<p> 
 </body>
 </html>
 
