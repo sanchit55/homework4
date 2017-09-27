9f7ef6d36a24c79f4917a3affb3108b1c01838bf
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-09-27 08:01:13 -0400

    second commit

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <body>
 <title>Week3 Assignment</title>
 <h1>Homework 3</h1>
-<h2>Adding Backslash in front of specified character</h2>
+<h2>Adding Backslash In Front of Specified Character</h2>
 <?php
-$str = addcslashes("Hi, My name is Sanchit Gupta","S");
+$str = addcslashes("Hi, My name is Sanchit Gupta and I am pursuing my masters
+in Information systems","S");
 echo($str);
+?>
+<h3>Get The Length Of The String of above mentioned syntax</h3>
+<?php
+echo strlen("Hi, My name is Sanchit Gupta and I am pursuing my masters in Information Systems");
 ?>
 </body>
 </html>
