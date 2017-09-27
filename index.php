f157284315de5434b575810a38f1fe73c5ba598e
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-09-27 09:21:25 -0400

    sixth commit

--- index.php
+++ index.php
@@ -29,11 +29,19 @@ I am pursuing my masters in Information Systems");
 ?>
 <p>" In this string function I replaced 'masters' to 'masters of science' from the
 above mentioned line."<p> 
+<hr>
 <h3>Repeating any particular number or name</h3>
 <?php
 echo str_repeat("5",10);
 ?>
 <p> In this string function I took 5 and got it repeated 10 times.</p>
+<hr>
+<h3>Comapring 2 strings</h3>
+<?php
+echo strcmp("I am doing IS601 assignment","I am doing IS601 assignment");
+?>
+<p> If this function outputs the value os "0", that means the strings are equal.
+And provides another value, that means they are not equal.</p>
 </body>
 </html>
 
