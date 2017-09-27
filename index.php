fefeb3ed15e096c0d5cdd16263c6911fc659cb9e
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-09-27 07:45:46 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<body>
+<title>Week3 Assignment</title>
+<h1>Homework 3</h1>
+<h2>Adding Backslash in front of specified character</h2>
+<?php
+$str = addcslashes("Hi, My name is Sanchit Gupta","S");
+echo($str);
+?>
+</body>
+</html>
+
